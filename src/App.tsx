c798299db22f5bb12179a59b418d8a5f90fb6890
Zer0 <iannicocaulin@gmail.com>
2026-03-17 17:28:51 +0800

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 20);
+      
     };
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
